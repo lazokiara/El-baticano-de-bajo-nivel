038ae080d4783bc404804061b0b5ae304ab6e146
Kiara <lazokiara4@gmail.com>
2024-05-24 10:56:43 -0300

    alumno

---
+++ alumno.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+typedef struct structEstudiante {
+    char nombre[100];
+    int edad;
+}Estudiante;
+
+typedef struct Materia{
+    char nombre[100];
+    int idMateria;
+   //  Estudiante estudiantes[]; 
+    // Materia correlativas[];
+}Materia;
