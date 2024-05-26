04458f0b686c1398edbaa67c04a054962668a5d4
Kiara <lazokiara4@gmail.com>
2024-05-26 12:49:51 -0300

    ingreso de datos

--- demo.c
+++ demo.c
@@ -4,13 +4,17 @@
 int main() {
 
     Lista* mi_lista = NewLista();
-    Estudiante *nacho = NewEstudiante("Ignacio",20);
+   // Estudiante *nacho = NewEstudiante("Ignacio",20);
     Estudiante *agus = NewEstudiante("Agustin",25);
-    
+    char nombre[10];
+    int edad;
     printf("Prueba\n");
-
-    printf("%s \n",nacho->nombre );
-    printf("%d \n",nacho->edad );
+    printf("Ingrese nombre y edad del estudiante a registrar:");
+    scanf("%s %d", nombre, &edad );
+    Estudiante *alumno = NewEstudiante(nombre, edad);
+    printf("Estudiante %s registrado correctamente \n",alumno->nombre );
+    inserInicio(mi_lista,alumno);
+    printf("Cantidad de alumnos: %d \n",mi_lista->size );
     
     printf("%s \n",agus->nombre );
     printf("%d \n",agus->edad );
--- structs/Materias.h
+++ structs/Materias.h
@@ -1,6 +1,17 @@
+#include <stdlib.h>
+//#include "ListaStructs/Lista.h"
+
 typedef struct Materia{
     char *nombre;
     int id_materia;
-    // Estudiante estudiantes[]; 
-    // Materia correlativas[];
+   // Lista *alumnos;
+    Materia *correlativas;
 }Materia;
+
+Materia *NewMateria(char *nom, int id){
+    Materia *materia;
+    materia = malloc(sizeof(Materia));
+    materia->id_materia = id;
+    materia->nombre = nom;
+    return materia;
+}
