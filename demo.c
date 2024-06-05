1e343008b951e27d1e247ae882ea32d5e7b85866
Ignacio Lobo <ignacioscarlobo@gmail.com>
2024-06-05 19:30:49 -0300

    Primer Menu

--- demo.c
+++ demo.c
@@ -1,52 +1,96 @@
 #include <stdio.h>
 #include "structs/ListaStructs/Lista.h"
 
-int main() {
+int main()
+{
 
-    Lista* mi_lista = NewLista();
-    Estudiante *nacho = NewEstudiante("Ignacio",20);
-    Estudiante *agus = NewEstudiante("Agustin",25);
-    char nombre[10];
-    int edad;
-    printf("Prueba\n");
-    printf("Ingrese nombre y edad del estudiante a registrar:");
-    gets(nombre);
-    scanf("%s %d", nombre, &edad );
-    Estudiante *alumno = NewEstudiante(nombre, edad);
-    printf("Estudiante %s registrado correctamente \n",alumno->nombre );
-    inserInicio(mi_lista,alumno);
-    printf("Cantidad de alumnos: %d \n",mi_lista->size );
-    
-    printf("%s \n",agus->nombre );
-    printf("%d \n",agus->edad );
+    int opcion = 0;
 
-    printf("\n\n");
+    Lista *mi_lista = NewLista();
 
-    Estudiante *kiara = NewEstudiante("Kiara",30);
+    while (opcion != 5)
+    {
+        printf("Ingrese la opcion de menu: \n 1: Ingresar Alumno. \n 5: Cerrar menu \n");
+        scanf("%d", &opcion);
+        switch (opcion)
+        {
+        case 0:
 
-    inserInicio(mi_lista,nacho);
-    inserInicio(mi_lista,agus);
-    inserFinal(mi_lista,kiara);
+            printf("Opcion 0");
+            char nombre[20];
+            int edad;
+            printf("Prueba\n");
+            printf("Ingrese nombre y edad del estudiante a registrar:");
+            // gets(nombre);
+            scanf("%s %d", nombre, &edad);
+            Estudiante *alumno = NewEstudiante(nombre, edad);
+            printf("Estudiante %s registrado correctamente \n", alumno->nombre);
+            inserInicio(mi_lista, alumno);
+            printf("Cantidad de alumnos: %d \n", mi_lista->size);
 
-    Estudiante* prueba = getEstudiantePorEdad(mi_lista,20);
+            
+            // scanf("%s %d", nombre, &edad);
+            break;
+        case 1:
+            printf("Opcion 1\n");
+            break;
+        case 2:
+            printf("Opcion 2\n");
+            break;
+        case 3:
+            printf("Cerrarndo menu\n");
+            break;
 
-    printf("%s", prueba->nombre);
+        case 5:
+            printf("Opcion 1\n");
+            printf("Cerrarndo menu\n");
+            break;
 
-    // Nodo* cursor = mi_lista->head;
-    // while (cursor!= NULL)
-    // {
-    //     /* code */
-    // }
-     
-
-    printf("el estudiante prueba %s \n", mi_lista->head->datos->nombre);
-    printf("su edad es: %d \n", mi_lista->head->datos->edad);
-    
-    printf("el estudiante prueba %s \n", mi_lista->tail->datos->nombre);
-    printf("su edad es: %d \n", mi_lista->tail->datos->edad);
-    
-   
-    
+        default:
+            printf("Opcion invalida");
+            break;
+        }
+    }
 
     return 0;
 }
+// Lista* mi_lista = NewLista();
+// Estudiante *nacho = NewEstudiante("Ignacio",20);
+// Estudiante *agus = NewEstudiante("Agustin",25);
+// char nombre[10];
+// int edad;
+// printf("Prueba\n");
+// printf("Ingrese nombre y edad del estudiante a registrar:");
+// gets(nombre);
+// scanf("%s %d", nombre, &edad );
+// Estudiante *alumno = NewEstudiante(nombre, edad);
+// printf("Estudiante %s registrado correctamente \n",alumno->nombre );
+// inserInicio(mi_lista,alumno);
+// printf("Cantidad de alumnos: %d \n",mi_lista->size );
+
+// printf("%s \n",agus->nombre );
+// printf("%d \n",agus->edad );
+
+// printf("\n\n");
+
+// Estudiante *kiara = NewEstudiante("Kiara",30);
+
+// inserInicio(mi_lista,nacho);
+// inserInicio(mi_lista,agus);
+// inserFinal(mi_lista,kiara);
+
+// Estudiante* prueba = getEstudiantePorEdad(mi_lista,20);
+
+// printf("%s", prueba->nombre);
+
+// // Nodo* cursor = mi_lista->head;
+// // while (cursor!= NULL)
+// // {
+// //     /* code */
+// // }
+
+// printf("el estudiante prueba %s \n", mi_lista->head->datos->nombre);
+// printf("su edad es: %d \n", mi_lista->head->datos->edad);
+
+// printf("el estudiante prueba %s \n", mi_lista->tail->datos->nombre);
+// printf("su edad es: %d \n", mi_lista->tail->datos->edad);
